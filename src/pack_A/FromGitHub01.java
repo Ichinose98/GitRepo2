c74f05c2819470ecbce554431f4e86c1be62abd5
Ichinose98 <ysk.ichinose@gmail.com>
2019-04-29 11:35:58 +0800

    29-Apr 11:35 merged!

--- src/pack_A/FromGitHub01.java
+++ src/pack_A/FromGitHub01.java
@@ -3,5 +3,10 @@ import java.util.*;
 public class FromGitHub01 {
 }
 class Local01{} 
-class Local02{}
 class Remote01{}
+class Remote02{}
+class Remote03{
+	void method () {
+		System.out.println("one more update!");//merged!
+	}
+}
