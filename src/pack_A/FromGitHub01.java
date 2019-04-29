f4cf13aa9a393956495c4a2b62d2f8178f66ded0
Ichinose98 <ysk.ichinose@gmail.com>
2019-04-29 14:11:30 +0800

    29-Apr 14:11 from local

--- src/pack_A/FromGitHub01.java
+++ src/pack_A/FromGitHub01.java
@@ -7,6 +7,12 @@ class Remote01{}
 class Remote02{}
 class Remote03{
 	void method () {
-		System.out.println("one more update!");//merged!
+		for (int i  = 0;i<5;i++) {
+			System.out.println("one more update!");//merged!
+		}
 	}  
-}  
+}
+class Outer01 {
+	static class Inner01 {
+	}
+}
