32f1bae42bfb763f2e81bb91793e5851648a30b9
Ichinose98 <49739699+Ichinose98@users.noreply.github.com>
2019-04-29 14:09:16 +0800

    Update FromGitHub01.java
    29-Apr 14:09 from remote

--- src/pack_A/FromGitHub01.java
+++ src/pack_A/FromGitHub01.java
@@ -1,12 +1,7 @@
 package pack_A;
 import java.util.*;
 public class FromGitHub01 {
+	void method01 (){
+		int a = 1;
+	}
 }
-class Local01{} 
-class Remote01{}
-class Remote02{}
-class Remote03{
-	void method () {
-		System.out.println("one more update!");//merged!
-	}  
-}  
