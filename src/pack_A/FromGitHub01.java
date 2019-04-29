b414b6e559b846f0efea603dd37f5ca21bc62b89
Ichinose98 <ysk.ichinose@gmail.com>
2019-04-29 11:02:36 +0800

    29-Apr 11:02 from local

--- src/pack_A/FromGitHub01.java
+++ src/pack_A/FromGitHub01.java
@@ -1,9 +1,4 @@
 package pack_A;
 import java.util.*;
-//19-Apr 09:41
-class FromLocal{}
-class FromLocal02{}
-class FromRemote{}
-class FromRemote02{}
 public class FromGitHub01 {
 }
--- src/pack_A/FromGitHub02.java
+++ src/pack_A/FromGitHub02.java
@@ -1,5 +1,5 @@
 package pack_A;
 import java.util.*;
-
+class FromLocal{}
 public class FromGitHub02 {
 }
